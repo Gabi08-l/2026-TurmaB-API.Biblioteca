4f2638715e87d62c0a178175e67bd774203caec5
53384866878 <53384866878@ECFP790D1350095>
2026-03-05 16:34:11 -0300

    Adicionar arquivos de projeto.

---
+++ API.Biblioteca/Models/Genero.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Biblioteca.Models
+{
+    public class Genero
+    {
+        //o id da classe deve ser o Nome +Id
+        public Guid GeneroId { get; set; }
+
+        //nome é obrigatório, ? so pra tirar o cerrilhado
+        [Required]
+        public string? Nome { get; set; }
+
+        //propriedade de navegação para o relacionamento muitos-para-muitos com Livro
+        public ICollection<Livro>? Livros { get; set; }
+
+    }
+}
---
+++ API.Biblioteca/Models/LivroGenero.cs
@@ -0,0 +1,20 @@
+﻿namespace API.Biblioteca.Models
+{
+    public class LivroGenero
+    {
+        public Guid LivroGeneroId { get; set; }
+        //fk
+        public Guid LivroId { get; set; }
+        //propriedades de navegação 
+
+        public Livro? Livro { get; set; }
+
+        //fk
+        public Guid GeneroId { get; set; }
+        //propriedades de navegação 
+
+        public Genero? Genero { get; set; }
+
+  
+    }
+}
